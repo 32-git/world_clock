50435d7caa9365262584d196a589ad987a91cb7a
32-git <110297700+32-git@users.noreply.github.com>
2022-08-11 11:40:52 +0200

    Changes to UI with CustomTkinter
    - Changes to the aesthetic of the buttons with CTk (rounded edges)
    - Title and button fonts changed to 'Audiowide' for uniqueness in design and appearance overall
    - Theme changed to blue theme, which currently only works in light mode, hence white background for now
        --> planned is a CTkSwitch to change between light/dark modes; colours to be optimised accordingly

--- world_clock.py
+++ world_clock.py
@@ -1,16 +1,18 @@
-from tkinter import *
+import tkinter as tk
+import tkinter.font as font
+import customtkinter as ctk
 from datetime import datetime
 import pytz
 
 # Prepare the GUI
-root = Tk()
+root = ctk.CTk()
 root.title("World Clock")
 root.geometry("600x400")
-root.config(bg="black")
+root.config(bg="white")
 
 # Lists to better assort values
 locations = ["Berlin, Germany", "Chennai, India", "London, United Kingdom", "New York City, USA", "Tokyo, Japan", "Los Angeles, USA"]
-time_zones = ['Europe/Berlin', 'Asia/Kolkata', 'Europe/London', 'America/New_York', 'Asia/Tokyo','America/Los_Angeles']
+timezones = ['Europe/Berlin', 'Asia/Kolkata', 'Europe/London', 'America/New_York', 'Asia/Tokyo','America/Los_Angeles']
 
 def clear_frame():
     for widget in root.winfo_children():
@@ -20,24 +22,24 @@ def buttons(location_num):
     clear_frame()
     root.geometry("400x200")  # change window size for appropriate fit
     t = locations[location_num]  # set location
-    buttons.tz = pytz.timezone(time_zones[location_num])  # set timezone
+    buttons.tz = pytz.timezone(timezones[location_num])  # set timezone
 
     # Prepare the labels
-    text = Label(root, text=t, font=("Arial", 15), relief='groove', padx=10, pady=7.5, 
-                    borderwidth=3, bg="black", fg="white")
-    text.place(relx=0.5, y=20, anchor=CENTER)
+    text = ctk.CTkLabel(root, text=t, text_font=("Arial", 15), padx=10, pady=7.5, 
+                    borderwidth=3, bg="white", fg="black")
+    text.place(relx=0.5, y=20, anchor='center')
 
-    buttons.time_label = Label(root, font=("Arial", 40), bg="black", fg="white")
-    buttons.time_label.pack(expand=True)
+    buttons.time_label = tk.Label(root, font=("Arial", 40), bg="white", fg="black")
+    buttons.time_label.place(relx=0.5, rely=0.5, anchor='center')
 
-    buttons.date_label = Label(root, font=("Arial", 11), bg="black", fg="white")
-    buttons.date_label.place(relx=0.5, y=175, anchor=CENTER)
+    buttons.date_label = tk.Label(root, font=("Arial", 11), bg="white", fg="black")
+    buttons.date_label.place(relx=0.5, y=175, anchor='center')
 
     update()
 
 def update():
     time = datetime.now(buttons.tz).strftime("%H:%M:%S")
-    date = datetime.now(buttons.tz).strftime(f"%a, %d.%m.%Y") 
+    date = datetime.now(buttons.tz).strftime(f"%A, %d.%m.%Y") 
 
     buttons.time_label.config(text=time)
     buttons.date_label.config(text=date)
@@ -45,35 +47,49 @@ def update():
 
 
 # Import the photos necessary for the (custom) buttons
-berlin_photo = PhotoImage(file='button_files\\berlin.png')
-chennai_photo = PhotoImage(file='button_files\\chennai.png')
-london_photo = PhotoImage(file='button_files\\london.png')
-new_york_photo = PhotoImage(file='button_files\\new_york.png')
-tokyo_photo = PhotoImage(file='button_files\\tokyo.png')
-la_photo = PhotoImage(file='button_files\\la.png')
+germany = tk.PhotoImage(file='button_files\\germany.png')
+india = tk.PhotoImage(file='button_files\\india.png')
+uk = tk.PhotoImage(file='button_files\\uk.png')
+usa = tk.PhotoImage(file='button_files\\usa.png')
+japan = tk.PhotoImage(file='button_files\\japan.png')
 
 # Make the title
-title = Label(root, text="World Clock", font=("Papyrus", 25, 'bold'), relief='groove', 
-                padx=10, pady=7.5, borderwidth=3, bg="black", fg="white")
-title.place(relx=0.5, rely=0.125, anchor=CENTER)
+title = ctk.CTkLabel(root, text="World Clock", text_font=("Audiowide", 25), padx=10, pady=7.5, 
+                        borderwidth=1.5, text_color='cyan4')
+title.place(relx=0.5, rely=0.125, anchor='center')
+
 
 # Make the buttons
-berlin = Button(root, image= berlin_photo, command=lambda: buttons(0))
-berlin.place(relx=0.225, rely=0.35, anchor=CENTER)
+button_font = font.Font(family="Arial", size=16)
 
-chennai = Button(root, image= chennai_photo, command=lambda: buttons(1))
-chennai.place(relx=0.775, rely=0.35, anchor=CENTER)
+berlin = ctk.CTkButton(root, corner_radius=10, image=germany, text="Berlin, Germany", text_font=("Audiowide", 15), 
+                        command=lambda: buttons(0), border_width=2, fg_color='LightCyan2', border_color= 'CadetBlue4', 
+                        text_color='SkyBlue4',  hover_color='LightSkyBlue2')
+berlin.place(relx=0.23, rely=0.35, anchor='center')
 
-london = Button(root, image= london_photo, command=lambda: buttons(2))
-london.place(relx=0.225, rely=0.6, anchor=CENTER)
+chennai = ctk.CTkButton(root, corner_radius=10, image=india, text="Chennai, India", text_font=("Audiowide", 15), 
+                        command=lambda: buttons(1), border_width=2, fg_color='LightCyan2', border_color= 'CadetBlue4', 
+                        text_color='SkyBlue4',  hover_color='LightSkyBlue1')
+chennai.place(relx=0.77, rely=0.35, anchor='center')
 
-new_york = Button(root, image= new_york_photo, command=lambda: buttons(3))
-new_york.place(relx=0.775, rely=0.6, anchor=CENTER)
+london = ctk.CTkButton(root, corner_radius=10, image=uk, text="London, UK", text_font=("Audiowide", 15), 
+                        command=lambda: buttons(2), border_width=2, fg_color='LightCyan2', border_color= 'CadetBlue4', 
+                        text_color='SkyBlue4',  hover_color='LightSkyBlue1')
+london.place(relx=0.23, rely=0.6, anchor='center')
 
-tokyo = Button(root, image= tokyo_photo, command=lambda: buttons(4))
-tokyo.place(relx=0.225, rely=0.85, anchor=CENTER)
+new_york = ctk.CTkButton(root, corner_radius=10, image=usa, text="New York, USA", text_font=("Audiowide", 15), 
+                        command=lambda: buttons(3), border_width=2, fg_color='LightCyan2', border_color= 'CadetBlue4', 
+                        text_color='SkyBlue4',  hover_color='LightSkyBlue1')
+new_york.place(relx=0.77, rely=0.6, anchor='center')
 
-la = Button(root, image= la_photo, command=lambda: buttons(5))
-la.place(relx=0.775, rely=0.85, anchor=CENTER)
+tokyo = ctk.CTkButton(root, corner_radius=10, image=japan, text="Tokyo, Japan", text_font=("Audiowide", 15), 
+                        command=lambda: buttons(4), border_width=2, fg_color='LightCyan2', border_color= 'CadetBlue4', 
+                        text_color='SkyBlue4',  hover_color='LightSkyBlue1')
+tokyo.place(relx=0.23, rely=0.85, anchor='center')
+
+la = ctk.CTkButton(root, corner_radius=10, image=usa, text="Los Angeles, USA", text_font=("Audiowide", 15), 
+                        command=lambda: buttons(5), border_width=2, fg_color='LightCyan2', border_color= 'CadetBlue4', 
+                        text_color='SkyBlue4',  hover_color='LightSkyBlue1')
+la.place(relx=0.77, rely=0.85, anchor='center')
 
 root.mainloop()
